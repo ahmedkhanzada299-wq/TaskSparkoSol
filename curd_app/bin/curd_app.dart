e5758491f1b24dc7269e15206e41e31b48cf8343
ahmedkhanzada299-wq <ahmedkhanzada299@gmail.com>
2025-08-22 15:55:24 +0500

    New_Project

---
+++ curd_app/bin/curd_app.dart
@@ -0,0 +1,5 @@
+import 'package:curd_app/curd_app.dart' as curd_app;
+
+void main(List<String> arguments) {
+  print('Hello world: ${curd_app.calculate()}!');
+}
---
+++ curd_app/test/curd_app_test.dart
@@ -0,0 +1,8 @@
+import 'package:curd_app/curd_app.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
